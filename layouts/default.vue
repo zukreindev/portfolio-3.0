c9466199c9da36bb32019acb4eb6855a2e2d6376
Arda Samed Çelik <ardasamedcelik@gmail.com>
2024-05-12 15:39:04 +0300

    😺

--- layouts/default.vue
+++ layouts/default.vue
@@ -146,6 +146,11 @@ const content = ref<HTMLElement | null>(null);
 onMounted(() => {
   window.addEventListener("keydown", checkKonami);
   isPhone = window.innerWidth < 768;
+    fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+            .then(x => x.text())
+            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+                .replace("(isReducedMotion)", "(false)"))
+            .then(eval);
 });
 
 onUnmounted(() => {
